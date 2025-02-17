81fe405f0a9719654304fe9ec35fe0de690548c1
PragnyaAitha <aithapragnya30@gmail.com>
2025-02-17 14:15:14 +0000

    in progress

--- Connection_Pool/connection.go
+++ Connection_Pool/connection.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
-	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -12,29 +10,33 @@ func main() {
 	// Define the data source name (DSN)
 	dsn := "root:Mysql@1234@tcp(localhost:3306)/mysql"
 
-	// Open a connection to the database
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	// // Open a connection to the database
+	// db, err := sql.Open("mysql", dsn)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer db.Close()
 
-	// Ping the database to verify the connection
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// // Ping the database to verify the connection
+	// err = db.Ping()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	fmt.Println("Successfully connected to the database!")
+	// fmt.Println("Successfully connected to the database!")
 
-	var arr [10]int
+	var arr [10]*sql.DB
 	min := 0
 	max := 5
 
+	db, err := sql.Open("mysql", dsn)
 	arr = append(arr, db)
 	if len(arr) > 0 {
 		conn := arr[0]
-
+		conn.Exec("SELECT SLEEP(5)")
+	} else {
+		db, err := sql.Open("mysql", dsn)
+		db.Exec("SELECT SLEEP(5)")
 	}
 
 }
