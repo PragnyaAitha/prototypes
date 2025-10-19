cc1d42bc83fbbdf80945a95f0067c33453653d0c
PragnyaAitha <aithapragnya30@gmail.com>
2025-10-19 17:52:46 +0000

    final changes

--- Connection_Pool/connection.go
+++ Connection_Pool/connection.go
@@ -2,41 +2,49 @@ package main
 
 import (
 	"database/sql"
+	"log"
+	"sync"
+	"time"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	// var arr [10]*sql.DB
+	// min := 1
+	// max := 5
+
 	// Define the data source name (DSN)
-	dsn := "root:Mysql@1234@tcp(localhost:3306)/mysql"
-
-	// // Open a connection to the database
-	// db, err := sql.Open("mysql", dsn)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer db.Close()
-
-	// // Ping the database to verify the connection
-	// err = db.Ping()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println("Successfully connected to the database!")
-
-	var arr [10]*sql.DB
-	min := 0
-	max := 5
+	dsn := "root:pass1234@tcp(localhost:3306)/mysql"
 
 	db, err := sql.Open("mysql", dsn)
-	arr = append(arr, db)
-	if len(arr) > 0 {
-		conn := arr[0]
-		conn.Exec("SELECT SLEEP(5)")
-	} else {
-		db, err := sql.Open("mysql", dsn)
-		db.Exec("SELECT SLEEP(5)")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer db.Close()
+	db.SetMaxOpenConns(10);
+	db.SetMaxIdleConns(5)
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("Cannot connect to DB:", err)
+	}
+
+	start := time.Now()
+	var wg sync.WaitGroup
+	numWorkers := 10000
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func(worker int) {
+			defer wg.Done()
+			_, err := db.Exec("SELECT SLEEP(0.01)")
+			if err != nil {
+				log.Println("Query error:", err)
+			}
+		}(i)
+	}
+	wg.Wait()
+	
+	fmt.Println("Job done", time.Since(start))
 
 }
